63a040dc20daec0b4becff0f21b3a043cca90466
Anil <abasdeo29@gmail.com>
2023-02-15 18:49:24 -0500

    truffle compiled initial contract

--- src/contracts/KryptoBirdz.sol
+++ src/contracts/KryptoBirdz.sol
@@ -1,33 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import './ERC721Connector.sol';
+contract KryptoBird {
 
-contract KryptoBird is ERC721Connector {
+    string private name;
+    string private symbol;
 
-    // array to store our nfts
-    string [] public kryptoBirdz;
-
-    mapping(string => bool) _kryptoBirdzExists;
-
-    function mint(string memory _kryptoBird) public {
-
-        require(!_kryptoBirdzExists[_kryptoBird],
-        'Error - kryptoBird already exists');
-        // this is deprecated - uint _id = KryptoBirdz.push(_kryptoBird);
-        kryptoBirdz.push(_kryptoBird);
-        uint _id = kryptoBirdz.length - 1;
-
-        // .push no longer returns the length but a ref to the added element
-        _mint(msg.sender, _id);
-
-        _kryptoBirdzExists[_kryptoBird] = true;
-
+    constructor() {
+        name = 'KryptoBird'; 
+        symbol = 'KBIRDZ';
     }
-
-    constructor() ERC721Connector('KryptoBird','KBIRDZ')
- {}
-
 }
-
-
